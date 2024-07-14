abb1d71dee02a284e8f09ab309939bbf490d4e05
Celal Özdemir <celal2344@gmail.com>
2024-07-14 22:28:30 +0300

    cleaning

--- src/components/NumberFilters.jsx
+++ src/components/NumberFilters.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 
 function NumberFilters({ setAgeSend, setEpisodesSend }) {
-    const [error, setError] = useState(null);
     const [age, setAge] = useState(0);
     const [episodes, setEpisodes] = useState(0);
 
@@ -15,15 +14,12 @@ function NumberFilters({ setAgeSend, setEpisodesSend }) {
     }, [episodes])
     return (
         <div>
-            {!error ? (
+            {
                 <div data-mdb-input-init className="form-outline row">
                     <input type="number" id="typeNumber" className="form-control m-2 col" placeholder="Age" onChange={(event) => setAge(event.target.value)} />
 
                     <input type="number" id="typeNumber" className="form-control m-2 col" placeholder="Number of Episodes" onChange={(event) => setEpisodes(event.target.value)} />
                 </div>
-            ) : (
-                <p className="error-message">{error}</p>
-            )
             }
         </div >
     )
